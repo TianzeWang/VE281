0cacba575b986dbd57f1030d33179e29a5b0d435
TianzeWang <wangtianze@sjtu.edu.cn>
2017-11-26 15:59:53 +0800

    Merge

--- Project3/cmake-build-debug/CMakeFiles/Project3.dir/cmake_clean.cmake
+++ Project3/cmake-build-debug/CMakeFiles/Project3.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Project3.dir/test_heap.cpp.o"
+  "CMakeFiles/Project3.dir/test_heap.cpp.obj"
   "Project3.pdb"
-  "Project3"
+  "Project3.exe"
+  "Project3.exe.manifest"
+  "libProject3.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
