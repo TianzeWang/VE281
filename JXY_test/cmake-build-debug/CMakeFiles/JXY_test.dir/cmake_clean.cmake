c24a2512d1c6c72b9d13210ee4fccaa2c80acb7f
Tianze Wang <wangtianze@sjtu.edu.cn>
2017-11-28 00:44:01 +0800

    debugged runtime error

---
+++ JXY_test/cmake-build-debug/CMakeFiles/JXY_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JXY_test.dir/main.cpp.o"
+  "JXY_test.pdb"
+  "JXY_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JXY_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
